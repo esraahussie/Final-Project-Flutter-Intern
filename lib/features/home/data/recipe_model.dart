0f4b200d54903c31f1127a5170faa67687a0c20d
Esraa Hussien <67114479+esraahussie@users.noreply.github.com>
2025-08-08 22:46:02 +0300

    Update recipe_model.dart

--- lib/features/home/data/recipe_model.dart
+++ lib/features/home/data/recipe_model.dart
@@ -1,1 +1,26 @@
+import '../domain/recipe_entity.dart';
+
+class RecipeModel extends RecipeEntity {
+  RecipeModel({
+    required super.title,
+    required super.category,
+    required super.ingredientsCount,
+    required super.description,
+    required super.ingredients,
+    required super.durationMinutes,
+    super.isFavorite,
+  });
+
+  factory RecipeModel.fromEntity(RecipeEntity entity) {
+    return RecipeModel(
+      title: entity.title,
+      category: entity.category,
+      ingredientsCount: entity.ingredientsCount,
+      description: entity.description,
+      ingredients: entity.ingredients,
+      durationMinutes: entity.durationMinutes,
+      isFavorite: entity.isFavorite,
+    );
+  }
+}
 
