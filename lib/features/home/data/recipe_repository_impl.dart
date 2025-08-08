9e1fcb39d9813f68974e3da5d240d1c992e6e466
Esraa Hussien <67114479+esraahussie@users.noreply.github.com>
2025-08-08 22:46:24 +0300

    Update recipe_repository_impl.dart

--- lib/features/home/data/recipe_repository_impl.dart
+++ lib/features/home/data/recipe_repository_impl.dart
@@ -1,1 +1,35 @@
+import '../domain/recipe_entity.dart';
+import '../domain/recipe_repository.dart';
+
+class RecipeRepositoryImpl implements RecipeRepository {
+  final List<RecipeEntity> _recipes = [];
+
+  @override
+  List<RecipeEntity> getAllRecipes() => _recipes;
+
+  @override
+  void addRecipe(RecipeEntity recipe) {
+    _recipes.add(recipe);
+  }
+
+  @override
+  void toggleFavorite(RecipeEntity recipe) {
+    final index = _recipes.indexOf(recipe);
+    if (index != -1) {
+      _recipes[index] = RecipeEntity(
+        title: recipe.title,
+        category: recipe.category,
+        ingredientsCount: recipe.ingredientsCount,
+        description: recipe.description,
+        ingredients: recipe.ingredients,
+        durationMinutes: recipe.durationMinutes,
+        isFavorite: !recipe.isFavorite,
+      );
+    }
+  }
+
+  @override
+  List<RecipeEntity> getFavoriteRecipes() =>
+      _recipes.where((r) => r.isFavorite).toList();
+}
 
