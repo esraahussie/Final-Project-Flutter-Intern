8c545731a573a8111ef993dc8b41ccc8d6cf0564
Esraa Hussien <67114479+esraahussie@users.noreply.github.com>
2025-08-08 22:47:24 +0300

    Update recipe_repository.dart

--- lib/features/home/domian/recipe_repository.dart
+++ lib/features/home/domian/recipe_repository.dart
@@ -1,1 +1,9 @@
+import 'recipe_entity.dart';
+
+abstract class RecipeRepository {
+  List<RecipeEntity> getAllRecipes();
+  void addRecipe(RecipeEntity recipe);
+  void toggleFavorite(RecipeEntity recipe);
+  List<RecipeEntity> getFavoriteRecipes();
+}
 
