0c0db25d8118ab53dc30364f0aa2b9d8e301b54f
Esraa Hussien <67114479+esraahussie@users.noreply.github.com>
2025-08-08 22:46:50 +0300

    Update add_recipe_usecase.dart

--- lib/features/home/domian/add_recipe_usecase.dart
+++ lib/features/home/domian/add_recipe_usecase.dart
@@ -1,1 +1,13 @@
+import 'recipe_entity.dart';
+import 'recipe_repository.dart';
+
+class AddRecipeUseCase {
+  final RecipeRepository repository;
+
+  AddRecipeUseCase(this.repository);
+
+  void call(RecipeEntity recipe) {
+    repository.addRecipe(recipe);
+  }
+}
 
